28f237e7d9b025376f82db81d9b45b3a628f9f4a
alter-c <83056486+alter-c@users.noreply.github.com>
2022-05-23 01:12:55 +0800

    Update ACO.py

--- ACO.py
+++ ACO.py
@@ -1,12 +1,16 @@
 import random
 import os
 import matplotlib.pyplot as plt
+from math import *
 
 os.system('cls')
 
 num_city = 6 # 城市数量
 num_ant = 20 # 蚂蚁数量
 rho = 0.5 # 信息素挥发率
+alpha = 1 # 信息素重要程度
+beta = 1 # 启发信息重要程度
+q = 1 # 信息素增强系数
 city = range(num_city) # 城市数组
 distance = [] # 城市距离矩阵
 pheromone = [] # 信息素矩阵
@@ -30,7 +34,7 @@ def set_pheromone():
     for i in range(num_city):
         pheromone.append([])
         for j in range(num_city):
-            p = 0 if i==j else 1/(num_city*(num_city-1))
+            p = 0 if i==j else q/(num_city*(num_city-1))
             pheromone[i].append(p)
 
 def roulette_select(current_city,unvisit_list):
@@ -38,12 +42,14 @@ def roulette_select(current_city,unvisit_list):
     possibility = []
     total_possibility = 0
     for next_city in unvisit_list:
-        possibility.append(pheromone[current_city][next_city])
+        # 下一节点概率(未归一) = 信息素^alpha * (1/距离)^(beta) 
+        p = pow(pheromone[current_city][next_city],alpha)*pow((1/distance[current_city][next_city]),beta)
+        possibility.append(p)
         total_possibility += possibility[-1]
         possibility[-1] = total_possibility 
 
     city = [0 for i in range(len(unvisit_list))] # 记录轮盘赌选到某一城市的次数
-    for num_rand in range(1):
+    for num_rand in range(5): # 轮盘转动次数
         rand = random.uniform(0,total_possibility)
         for i in range(len(possibility)):
             if rand <= possibility[i]:
@@ -60,9 +66,9 @@ def update_pheromone(list):
         path_list.append((list[k],list[k+1]))
     for i in range(num_city):
         for j in range(num_city):
-            if (i,j) in path_list:
-                pheromone[i][j] = (1-rho)*pheromone[i][j] + rho/len(path_list)
-            else:
+            if (i,j) in path_list: # 该路径在最短路线里:信息素增强
+                pheromone[i][j] = (1-rho)*pheromone[i][j] + q*rho/len(path_list)
+            else:   # 该路径不在最短路线里:信息素挥发
                 pheromone[i][j] = (1-rho)*pheromone[i][j]
 
 def ant_optimize(best_list,min_distance):
@@ -100,9 +106,9 @@ def plot(distance_list):
     plt.plot(iter,distance_list,color='r')
     plt.show()
 
-
 set_pheromone()
 set_distance()
+
 min_distance = 1e5 #  最短路程
 best_list = [] # 最优路线
 distance_list = [] # 记录最短路程变化
